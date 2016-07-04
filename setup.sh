fcdb89926f8ef41d237c20c3e4178a5f60776b3c
nic96 <jeromyreimer@gmail.com>
2016-07-04 08:54:35 -0600

    initial commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp -r ./resources/i3* ~/.config/
+cp ./resources/.Xresources ~/
+echo "Be sure into install all packages in the i3 group plus
+dmenu feh qlipper qt5ct i3blocks sysstat alsa-utils rxvt-unicode network-manager-applet
+The previous line can be copied and pasted after yaourt -S to install
+or if you don't want to install from aur you'll have to replace i3blocks with i3status in
+your ~/.config/i3/config and you won't be able to use qlipper
+Also be sure to export XDG_CURRENT_DESKTOP=kde and configure the look of qt applications via systemsettings5."
