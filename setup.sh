06d7659b02fe36ef4cfb1e49db27869cb4033d83
nic96 <jeromyreimer@gmail.com>
2016-12-29 22:43:49 -0600

    updated instructions

--- setup.sh
+++ setup.sh
@@ -6,4 +6,6 @@ dmenu feh qlipper qt5ct i3blocks sysstat alsa-utils rxvt-unicode network-manager
 The previous line can be copied and pasted after yaourt -S to install
 or if you don't want to install from aur you'll have to replace i3blocks with i3status in
 your ~/.config/i3/config and you won't be able to use qlipper
-Also be sure to export XDG_CURRENT_DESKTOP=kde and configure the look of qt applications via systemsettings5."
+Also be sure to export XDG_CURRENT_DESKTOP=kde and configure the look of qt applications via systemsettings5.
+Also to change the default terminal to Konsole export TERMINAL=konsole.
+To get keepassx to work you need to start it with env XDG_CURRENT_DESKTOP=gtk2 keepassx"
